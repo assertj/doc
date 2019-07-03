67a5f5748e1e8eb2ad9df35bac1c2b777a51fa25
Joel Costigliola <joel.costigliola@gmail.com>
2019-07-03 22:19:01 +1200

    Document assumptions

--- src/test/java/example/core/AssumptionsDemo.java
+++ src/test/java/example/core/AssumptionsDemo.java
@@ -1,16 +1,29 @@
 package example.core;
 
 // @formatter:off
-// tag::user_guide[]
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
-import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 import org.junit.jupiter.api.Test;
 
 class AssumptionsDemo {
 
+  // tag::assumption_not_met[]
+  @Test
+  public void when_an_assumption_is_not_met_the_test_is_ignored() {
+    // since this assumption is obviously false ...
+    assumeThat(frodo.getRace()).isEqualTo(ORC);
+    // ... this assertion is not performed
+    assertThat(fellowshipOfTheRing).contains(sauron);
+  }
+  // end::assumption_not_met[]
 
+  // tag::assumption_met[]
+  @Test
+  public void when_all_assumptions_are_met_the_test_is_run_normally() {
+    // since this assumption is true ...
+    assumeThat(frodo.getRace()).isEqualTo(HOBBIT);
+    // ... this assertion is performed
+    assertThat(fellowshipOfTheRing).doesNotContain(sauron);
+  }
+  // end::assumption_met[]
 }
-// end::user_guide[]
 // @formatter:on
